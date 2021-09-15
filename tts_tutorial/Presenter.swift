ae1e4a991051cdb1e5df0a4ded04cfb92780f8bc
tomoyukikoyanagi <tomoyukikoyanagi@gmail.com>
2021-09-15 14:13:59 +0900

    first commit

---
+++ tts_tutorial/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  tts_tutorial
+//
+//  Created by 小柳智之 on 2021/09/15.
+//
+
+import Foundation
---
+++ tts_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tts_tutorial
+//
+//  Created by 小柳智之 on 2021/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
