84584006f9d8dfbf657d5c88db563bab8755b627
tomoyukikoyanagi <tomoyukikoyanagi@gmail.com>
2021-09-15 16:02:35 +0900

    ui branch

--- tts_tutorial/Presenter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Presenter.swift
-//  tts_tutorial
-//
-//  Created by 小柳智之 on 2021/09/15.
-//
-
-import Foundation
--- tts_tutorial/ViewController.swift
+++ tts_tutorial/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var speechText: UITextField!
+
+    @IBOutlet weak var startSpeechButton: UIButton!
+    
+    private var input: TTSPresenterInput!
+    func inject(input: TTSPresenter){
+        self.input = input
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        //view.addSubview(startSpeechButton)
+        //view.addSubview(speechText)
+
+        //startSpeechButton.addTarget(self, action: #selector(speechButtonOnTap), for:.touchDown)
+        //speechText.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    @IBAction func speechButtonOnTap(_ sender: Any) {
+        guard let text = speechText.text else {
+            return
+        }
+        input.speechButtonOnTap(text: text)
+    }
+    
+    @IBAction func textFieldDidChange(_ sender: Any) {
+        guard let text = speechText.text else {
+            return
+        }
+        input.textDidChange(text: text)
     }
 
+}
 
+extension ViewController: TTSPresenterOutput {
+    func changeSpeechButtonEnable(isEnable: Bool) {
+        startSpeechButton.isEnabled = isEnable
+    }
+    
+    func showErrorAlert(title: String, message: String) {
+        let actionAlert = UIAlertController (title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title:"close", style: .cancel, handler: nil)
+        actionAlert.addAction(cancelAction)
+        self.present(actionAlert, animated:true, completion: nil)
+    }
 }
 
